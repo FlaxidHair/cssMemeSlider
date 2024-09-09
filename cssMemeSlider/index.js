9dd108ecc461494238e7e0091d051befb9e2020e
Flaxid Hair <kmarlou@icloud.com>
2024-09-09 12:09:10 +0300

    feat: add transition to images  (Mon, Sep 09, 2024 12:09:10 PM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -4,10 +4,18 @@ const sliderContent = document.querySelector(".slider-wrapper__content");
 
 paginationItems.forEach((el) => {
   el.addEventListener("click", () => {
-    paginationItems.forEach((elem) => {
-      elem.children[0].classList.remove("active");
-    });
-    el.children[0].classList.add("active");
-    sliderContent.innerHTML = memes[paginationItems.indexOf(el)].outerHTML;
+    sliderContent.style.transition = "0.3s";
+    sliderContent.style.opacity = 0;
+    setTimeout(() => {
+      paginationItems.forEach((elem) => {
+        elem.children[0].classList.remove("active");
+      });
+      el.children[0].classList.add("active");
+      sliderContent.innerHTML = memes[paginationItems.indexOf(el)].outerHTML;
+    }, 250);
+
+    setTimeout(() => {
+      sliderContent.style.opacity = 1;
+    }, 300);
   });
 });
