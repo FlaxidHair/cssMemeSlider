40d5166f0606042c0df9acff8a669aa53db66378
Flaxid Hair <kmarlou@icloud.com>
2024-09-09 14:33:01 +0300

    feat: add responsive marks of slider and change px to em / % and add title for slider  (Mon, Sep 09, 2024 02:33:01 PM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,21 +1,28 @@
 const memes = [...document.querySelectorAll(".slider-content-meme")];
 const paginationItems = [...document.querySelectorAll(".pagination-wrap")];
 const sliderContent = document.querySelector(".slider-wrapper__content");
-
+const message = document.querySelector(".slider-wrapper__text");
+const messageText = ["Первое", "Второе", "Третье", "Четвертое"];
 paginationItems.forEach((el) => {
   el.addEventListener("click", () => {
-    sliderContent.style.transition = "0.3s";
+    if (el.children[0].classList.contains("active")) {
+      return;
+    }
     sliderContent.style.opacity = 0;
+    message.style.opacity = 0;
+
     setTimeout(() => {
       paginationItems.forEach((elem) => {
         elem.children[0].classList.remove("active");
       });
       el.children[0].classList.add("active");
+      message.textContent = messageText[paginationItems.indexOf(el)];
       sliderContent.innerHTML = memes[paginationItems.indexOf(el)].outerHTML;
     }, 250);
 
     setTimeout(() => {
       sliderContent.style.opacity = 1;
+      message.style.opacity = 1;
     }, 300);
   });
 });
